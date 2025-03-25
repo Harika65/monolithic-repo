acfa69dca9949bb1a8173f78d78d617467ac23f7
Harika <76474031+Harika65@users.noreply.github.com>
2025-03-25 20:30:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_template" "web_server_as" {
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
      listener {
-      instance_port     = 8000
+      instance_port     = 80
       instance_protocol = "http"
       lb_port           = 80
       lb_protocol       = "http"
